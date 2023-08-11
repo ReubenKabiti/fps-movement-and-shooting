c5137286435eefcdde2115791bc05c47e9cee0a2
ReubenKabiti <reubenrk2@gmail.com>
2023-08-11 07:28:55 +0300

    jumping and falling

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,12 @@ public class PlayerMovement : MonoBehaviour
     float _xRot = 0; // the rotation of the camera around the x axis
     CharacterController _controller;
 
+
+    // falling and jumping
+    public float gravityStrength = 9.8f;
+    public float jumpStrength = 10.1f;
+    Vector3 _velocity = Vector3.zero;
+
     void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -42,5 +48,20 @@ public class PlayerMovement : MonoBehaviour
         move *= speed * Time.deltaTime;
         _controller.Move(move);
 
+
+        // jumping and falling
+        _controller.Move(_velocity * Time.deltaTime);
+        if (_controller.isGrounded)
+        {
+            _velocity.y = 0;
+            if (Input.GetButton("Jump"))
+            {
+                _velocity.y += jumpStrength;
+            }
+        }
+        else
+        {
+            _velocity.y -= gravityStrength * Time.deltaTime;
+        }
     }
 }
