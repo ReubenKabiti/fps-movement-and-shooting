35d644bcfd7c87eb949f9ce6742199bccb811594
ReubenKabiti <reubenrk2@gmail.com>
2023-08-11 12:31:26 +0300

    enemies

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerFollower : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform player;
+    public float seekDistance = 3;
+    public float attackDistance = 2;
+
+    Vector3 returnPoint;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        returnPoint = transform.position;
+    }
+
+
+    void Update()
+    {
+        float distance = (transform.position - player.position).magnitude;
+        if (distance <= seekDistance && distance > attackDistance)
+            agent.destination = player.position;
+        else if (distance <= attackDistance)
+        {
+            // attack here
+            agent.destination = transform.position;
+        }
+        else
+        {
+            agent.destination = returnPoint;
+        }
+    }
+}
